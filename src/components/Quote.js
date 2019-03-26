57ff14de462e8beed73601faaced12512c334eec
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-03-26 00:58:18 -0600

    removed react boilerplate added simple file structure

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/Quote.css';
+
+class Quote extends React.Component {
+    render(){
+        return(
+            <div className='quote-container'>Quote Goes Here</div>
+        )
+    }
+}
+
+export default Quote;
