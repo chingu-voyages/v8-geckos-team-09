4f5cc70cba70761f6085901ce61573556c3ef514
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-03-29 01:14:43 -0600

    display todo list

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-    render(){
-        return(
-            <div className="todo-item">
-                <input type="checkbox" />
-                <p>Placeholder text here</p>
-            </div>
-        )
+function TodoItem(props) {
+    const completedStyle = {
+        color: "#cdcdcd",
+        textDecoration: "line-through"
     }
+
+    return (
+        <div className="todo-item">
+            <input 
+                type="checkbox" 
+                checked={props.item.completed}
+                onChange={() => props.handleChange(props.item.id)}
+            />
+            <p style={props.item.completed ? completedStyle: null}>{props.item.text}</p>
+        </div>
+    )
 }
 
                 
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,11 +3,61 @@ import '../style/Todos.css';
 import TodoItem from './TodoItem';
 
 class Todos extends React.Component {
+    
+    constructor(props){
+        super(props);
+    
+        this.state = {
+            todos: [
+                {
+                    id: 1,
+                    text: "Take out the trash",
+                    completed: true
+                },
+                {
+                    id: 2,
+                    text: "Grocery shopping",
+                    completed: false
+                },
+                {
+                    id: 3,
+                    text: "Clean gecko tank",
+                    completed: false
+                }
+            ]
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
+    addTodo(event) {
+        console.log(event.target.value)
+    }
+    
     render(){
+
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>)
+
         return(
             <div className='todos-container'>
-                <TodoItem />
-                <TodoItem />
+                <form onSubmit={this.addTodo}>
+                    <input placeholder="add todo..."></input>
+                    <button>Submit</button>
+                </form>
+                {todoItems}
             </div>
         )
     }
