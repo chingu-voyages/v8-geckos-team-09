515cdb1d119f312057e98ce875e73ce52ee7418d
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-03-29 01:30:15 -0600

    added clock

--- src/App.js
+++ src/App.js
@@ -5,9 +5,44 @@ import Weather from './components/Weather';
 import './App.css';
 
 class App extends React.Component {
+  
+  constructor() {
+    super()
+    
+    let d = new Date()
+      this.state = {
+        day: d.getDay(),
+        month: d.getMonth(),
+        date: d.getDate(),
+        year: d.getFullYear(),
+        time: d.toLocaleTimeString()
+      }
+    this.countingSecond = this.countingSecond.bind(this)
+  }
+  countingSecond() {
+    let d = new Date()
+    this.setState({
+      day: d.getDay(),
+      month: d.getMonth(),
+      date: d.getDate(),
+      year: d.getFullYear(),
+      time: d.toLocaleTimeString()
+    })
+  }
+  componentWillMount() {
+    setInterval(this.countingSecond, 1000)
+  }
+  
   render() {
+
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"]
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
     return (
       <div className="App">
+        <div className='clock-container'>
+          <h3 className='clock-text'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year} {this.state.time}</h3>
+        </div>
         <Todos />
         <Quote />
         <Weather />
