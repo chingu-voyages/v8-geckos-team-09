6a044561d9180bd72b15e7b44d2c82a5376a11ab
Maxwell Morgan <maxwelltmorgan@gmail.com>
2019-04-11 00:11:13 -0600

    style App.css

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import React from "react";
 
 import Quote from "./components/Quote";
 import Weather from "./components/WeatherApiCall";
-
-import "./App.css";
-
 import Todos from "./components/Todos";
-
 import "./App.css";
+import AAA from "./backgrounds/aaa.jpg";
+import BBB from "./backgrounds/bbb.jpg";
+import CCC from "./backgrounds/ccc.jpg";
+import DDD from "./backgrounds/ddd.jpg";
+import EEE from "./backgrounds/eee.jpg";
+import FFF from "./backgrounds/fff.jpg";
+import GGG from "./backgrounds/ggg.jpg";
+import HHH from "./backgrounds/hhh.jpg";
+import III from "./backgrounds/iii.jpg";
+import JJJ from "./backgrounds/jjj.jpg";
 
 class App extends React.Component {
   constructor() {
@@ -15,23 +21,22 @@ class App extends React.Component {
     
     let d = new Date()
       this.state = {
-        day: d.getDay(),
-        month: d.getMonth(),
-        date: d.getDate(),
-        year: d.getFullYear(),
         time: d.toLocaleTimeString(),
+        greeting: "Hello",
         divStyle: {
-          backgroundImage: "url(https://cdn.pixabay.com/photo/2018/08/12/15/29/hintersee-3601004_960_720.jpg)"
+          backgroundImage: `url(${AAA})`
         },
         backgrounds: [
-          "https://cdn.pixabay.com/photo/2018/08/12/15/29/hintersee-3601004_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2017/01/11/14/56/ireland-1971997_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2018/07/05/22/16/panorama-3519309_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2018/09/19/09/04/landscape-3688040_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2017/12/23/19/00/autumn-3035783_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2017/11/08/21/13/autumn-2931644_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2016/10/26/19/36/nature-1772432_960_720.jpg",
-          "https://cdn.pixabay.com/photo/2018/12/26/15/07/stykkisholmur-3895953_960_720.jpg"
+          AAA,
+          BBB,
+          CCC,
+          DDD,
+          EEE,
+          FFF,
+          GGG,
+          HHH,
+          III,
+          JJJ
         ]
       }
     this.countingSecond = this.countingSecond.bind(this);
@@ -41,18 +46,25 @@ class App extends React.Component {
   setBackground(){
     let divStyle = {...this.state.divStyle}
 
-    divStyle.backgroundImage = `url(${this.state.backgrounds[Math.floor(Math.random() * 8)]})`
+    divStyle.backgroundImage = `url(${this.state.backgrounds[Math.floor(Math.random() * 10)]})`
     this.setState({divStyle})
   }
   
   countingSecond() {
     let d = new Date();
+    let greeting;
+    
+    if (d.getHours() < 10) {
+      greeting = "Good morning";
+    } else if (d.getHours() < 20) {
+      greeting = "Good day";
+    } else {
+      greeting = "Good evening";
+    }
+
     this.setState({
-      day: d.getDay(),
-      month: d.getMonth(),
-      date: d.getDate(),
-      year: d.getFullYear(),
-      time: d.toLocaleTimeString()
+      time: d.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' }),
+      greeting: greeting
     });
   }
 
@@ -61,39 +73,18 @@ class App extends React.Component {
   }
 
   render() {
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "November",
-      "December"
-    ];
-    const days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday"
-    ];
-
     return (
       <div className="App" style = {this.state.divStyle}>
-        <button className="background-button" onClick={this.setBackground}>change background</button>
-        <div className='clock-container'>
-          <h2 className='clock-time'>{this.state.time}</h2>
-          <h3 className='clock-date'>{days[this.state.day]}, {months[this.state.month]} {this.state.date}, {this.state.year}</h3>
-        </div>
-        <Todos />
-        <Quote />
-        <Weather />
+        <main className="app-inner">
+          <button className="background-button" onClick={this.setBackground}>change background</button>
+          <div className='clock-container'>
+            <h2 className='clock-time'>{this.state.time}</h2>
+            <h2 className='greeting'>{this.state.greeting}!</h2>
+          </div>
+          <Quote />
+          <Todos />
+          <Weather />
+        </main>
       </div>
     );
   }
